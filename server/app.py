ca4ca4687ac1dabdf5a3f45fe451e97870c2c1d2
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-10-26 01:30:31 -0300

    implementa variables de entorno (.env) y logging

--- server/app.py
+++ server/app.py
@@ -1,8 +1,22 @@
+# import logging
+
 from fastapi import FastAPI
 
 from .api import api_router
 
+
+# logger = logging.getLogger(__name__)
 fast_projects = FastAPI()
 
 # Incluimos el router principal a la instancia de FastAPI
 fast_projects.include_router(api_router)
+
+
+# @fast_projects.on_event('startup')
+# async def startup_event():
+#     logger.debug('API Iniciada')
+
+
+# @fast_projects.on_event('shutdown')
+# def shutdown_event():
+#     logger.debug('API Finalizada')
--- server/controller/projects_controller.py
+++ server/controller/projects_controller.py
@@ -1,9 +1,12 @@
+import logging
 from typing import List
-
 
 from server.schemas.project_schemas import NewProjectRequest, ProjectResponse, ProjectRequest
 from server.exceptions import BaseHTTPException, InternalServerError
 from server.service import ProjectsService
+
+
+logger = logging.getLogger(__name__)
 
 
 class ProjectsController:
@@ -12,50 +15,55 @@ class ProjectsController:
 
     def create(self, new_project: NewProjectRequest) -> ProjectResponse:
         try:
+            logger.debug(f'Crear proyecto {new_project.title}')
             return self.service.create(new_project)
         except BaseHTTPException as ex:
-            # TODO: implementar logging
-            raise ex
+            self.__handler_http_exception(ex)
         except Exception:
-            # TODO log: Error no contemplado en ProjectsController.create
+            logger.critical(f'Error no contemplado en {__name__}.create()')
             raise InternalServerError()
 
     def get_list(self, limit: int, offset: int) -> List[ProjectResponse]:
         try:
             return self.service.get_list(limit, offset)
         except BaseHTTPException as ex:
-            # TODO: implementar logging
-            raise ex
+            self.__handler_http_exception(ex)
         except Exception:
-            # TODO log: Error no contemplado en ProjectsController.create
+            logger.critical(f'Error no contemplado en {__name__}.get_list()')
             raise InternalServerError()
 
     def get_by_id(self, id: int) -> ProjectResponse:
         try:
+            logger.debug(f'Buscar proyecto #{id}')
             return self.service.get_by_id(id)
         except BaseHTTPException as ex:
-            # TODO: implementar logging
-            raise ex
+            self.__handler_http_exception(ex)
         except Exception:
-            # TODO log: Error no contemplado en ProjectsController.create
+            logger.critical(f'Error no contemplado en {__name__}.get_by_id()')
             raise InternalServerError()
 
     def update(self, id: int, new_data: ProjectRequest) -> ProjectResponse:
         try:
             return self.service.update(id, new_data)
         except BaseHTTPException as ex:
-            # TODO: implementar logging
-            raise ex
+            self.__handler_http_exception(ex)
         except Exception:
-            # TODO log: Error no contemplado en ProjectsController.create
+            logger.critical(f'Error no contemplado en {__name__}.update()')
             raise InternalServerError()
 
     def delete(self, id: int) -> None:
         try:
             self.service.delete(id)
         except BaseHTTPException as ex:
-            # TODO: implementar logging
-            raise ex
+            self.__handler_http_exception(ex)
         except Exception:
-            # TODO log: Error no contemplado en ProjectsController.create
+            logger.critical(f'Error no contemplado en {__name__}.delete()')
             raise InternalServerError()
+
+    def __handler_http_exception(self, ex: BaseHTTPException):
+        if ex.status_code >= 500:
+            logger.critical(f'Error en el servidor con status code {
+                            ex.status_code}: {ex.description}')
+        else:
+            logger.error(f'Error {ex.status_code}: {ex.description}')
+        raise ex
