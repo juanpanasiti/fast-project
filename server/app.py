7b69d96f69a08110e970e26214acd289aacc5d36
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-11-15 23:44:49 -0300

    agrega api description a la docu

--- server/app.py
+++ server/app.py
@@ -6,6 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from .api import api_router
 from .database import db_connection
+from .configs import api_description
 from .middlewares import RequestLoggingMiddleware, JwtMiddleware
 
 
@@ -24,7 +25,10 @@ api_middlewares = [
 
 
 # logger = logging.getLogger(__name__)
-fast_projects = FastAPI(middleware=api_middlewares)
+fast_projects = FastAPI(
+    middleware=api_middlewares,
+    **api_description
+    )
 
 # Incluimos el router principal a la instancia de FastAPI
 fast_projects.include_router(api_router)
--- server/configs/__init__.py
+++ server/configs/__init__.py
@@ -1,5 +1,6 @@
 from .settings import Settings
 from .logging import config_logger
+from .api_doc import api_description
 
 app_settings = Settings()
 config_logger(app_settings.DEBUG)
